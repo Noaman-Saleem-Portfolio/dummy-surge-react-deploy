058de9dcb37f4484107073db950f7344cd4dfb72
Noaman-Saleem <noaman.saleem@umt.edu.pk>
2023-08-09 17:49:59 +0500

    hello from App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Noaman Saleem</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bootcamp 2023
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello From App.js</h1>;
 }
 
 export default App;
